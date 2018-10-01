a65811e0c30d2d270ae3c59ebd4dec3eba088d3e
Aaron Collier <aaron.collier@stanford.edu>
2018-10-01 10:17:43 -0700

    Simplify duplication logic

--- runtime/handler.go
+++ runtime/handler.go
@@ -139,14 +139,14 @@ func removeDuplicates(elements []string) []string {
 	result := []string{}
 
 	for v := range elements {
-		if encountered[elements[v]] == true {
+		if encountered[elements[v]] {
 			// Do not add duplicate.
-		} else {
-			// Record this element as an encountered element.
-			encountered[elements[v]] = true
-			// Append to result slice.
-			result = append(result, elements[v])
+			continue
 		}
+		// Record this element as an encountered element.
+		encountered[elements[v]] = true
+		// Append to result slice.
+		result = append(result, elements[v])
 	}
 	// Return the new slice.
 	return result
