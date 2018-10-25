b0a665870220ef262d2e888c0fd7398ca02452ed
Aaron Collier <aaron.collier@stanford.edu>
2018-10-25 11:59:35 -0700

    Add tests and complete complex query parsing

--- handler.py
+++ handler.py
@@ -1,6 +1,9 @@
 
 import os
 import rdflib
+from rdflib.plugins.sparql.parser import parseUpdate
+from rdflib.plugins.sparql.update import evalUpdate
+from rdflib.plugins.sparql.algebra import translateQuery, translateUpdate
 
 from sns_client import SnsClient
 from neptune_client import NeptuneClient
@@ -12,26 +15,54 @@ def main(event, context):
     rialto_sns_endpoint = os.getenv('RIALTO_SNS_ENDPOINT', "http://localhost:4575")
     rialto_topic_arn = os.getenv('RIALTO_TOPIC_ARN', "rialto")
     aws_region = os.getenv('AWS_REGION', "us-west-2")
+
     sns_client = SnsClient(rialto_sns_endpoint, rialto_topic_arn, aws_region)
     neptune_client = NeptuneClient(rialto_sparql_endpoint, rialto_sparql_path)
 
     response = neptune_client.post(event)
 
-    print("RESPONSE")
-    print(response)
-
     if response['statusCode'] == 200:
         sns_response = sns_client.publish(formatMessage(event['body']))
-        return sns_response
 
-    return ""
+    return {
+        'message' : response['body'],
+        'statusCode' : response['statusCode']
+	}
 
 def formatMessage(body):
-    graph = rdflib.Graph()
-    graph.update(body)
     subjects = []
+    graph = translateUpdate(parseUpdate(body))
+    for block in graph:
+        for key in block.keys():
+            if key in ['delete', 'insert']:
+                subjects += getSubjectsFromQuads(block[key]['quads'])
+                subjects += getSubjectsFromTriples(block[key]['triples'])
+            if key in ['quads']:
+                subjects += getSubjectsFromQuads(block['quads'])
+            if key in ['triples']:
+                subjects += getSubjectsFromTriples(block['triples'])
 
-    for s, _p, _o in graph:
+    return "{'Action': 'touch', 'Entities': %s}" % getUniqueSubjects(subjects)
+
+def getSubjectsFromQuads(block):
+    subjects = []
+    for key in block.keys():
+        for s, _p, _o in block[key]:
+            subjects.append(s.toPython())
+    
+    return subjects
+
+def getSubjectsFromTriples(block):
+    subjects = []
+    for s, _p, _o in block:
         subjects.append(s.toPython())
 
-    return "{'Action': 'touch', 'Entities': subjects}"
+    return subjects
+
+def getUniqueSubjects(subjectsList):
+    unique_subjects = []
+    for subject in subjectsList:
+        if subject not in unique_subjects:
+            unique_subjects.append(subject)
+    
+    return unique_subjects
