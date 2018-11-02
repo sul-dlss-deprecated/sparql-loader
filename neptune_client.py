92e21c3c63248da4b6aa8aeb69fdfaaa71d0af87
Justin Littman <justinlittman@stanford.edu>
2018-11-02 10:30:19 -0400

    Explicitly set utf-8 encoding in neptune_client.
    closes #75

--- neptune_client.py
+++ neptune_client.py
@@ -7,6 +7,6 @@ class NeptuneClient():
 
     def post(self, request_body, request_content_type):
         response = requests.post(self.sparql_endpoint,
-                                 data=request_body,
+                                 data=request_body.encode('utf-8'),
                                  headers={"Content-Type": request_content_type})
         return response.text, response.status_code
